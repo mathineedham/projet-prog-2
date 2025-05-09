6cbfc5b615462e0b2148d471fec9ee2b69d5205b
mathineedham <mathilde.needham@universite-paris-saclay.fr>
2025-05-09 16:11:52 +0200

    partie 1 : premier rendu

--- lib/uninitialized_places.ml
+++ lib/uninitialized_places.ml
@@ -14,12 +14,14 @@ let go prog mir : analysis_results =
   (* The set of all places appearing in the MIR code. We are not interesting in initializedness for places
     which are not member of this set. *)
   let all_places =
+    (* The set of local places*)
     let acc =
       Hashtbl.fold
         (fun l _ acc -> PlaceSet.add (PlLocal l) acc)
         mir.mlocals PlaceSet.empty
     in
-    Array.fold_left
+    (* We add all non local places to it*)
+    let a = Array.fold_left
       (fun acc (i, _) ->
         match i with
         | Ideinit _ | Igoto _ | Ireturn -> acc
@@ -33,7 +35,13 @@ let go prog mir : analysis_results =
             | RVmake (_, pls) -> PlaceSet.add_seq (List.to_seq pls) acc
             | RVunit | RVconst _ -> acc))
       acc mir.minstrs
+      in 
+      a
   in
+  
+  (* Printf.printf "All places:\n";
+  PlaceSet.iter (fun p -> Print_minimir.pp_place ( Format.std_formatter) p ) all_places;
+  Printf.printf "\n"; *)
 
   (* The set of subplaces of a given place. *)
   let subplaces = Hashtbl.create 7 in
@@ -44,6 +52,17 @@ let go prog mir : analysis_results =
         Hashtbl.add subplaces pl pls)
       all_places
   in
+
+  (* Printf.printf "Subplaces:\n";
+  Hashtbl.iter ( fun pl pls -> 
+    (Print_minimir.pp_place (Format.std_formatter) pl;
+    Printf.printf " -> ";
+      PlaceSet.iter (fun p -> Print_minimir.pp_place ( Format.std_formatter) p ) pls;
+       Printf.printf "\n");
+    )
+    subplaces;
+  Printf.printf "\n"; *)
+
 
   (* Effect of initializing a place [pl] on the abstract state [state]: [pl] and all its subplaces
     become initialized. Hence, given that the state is the set of uninitialized places, we remove
@@ -59,28 +78,72 @@ let go prog mir : analysis_results =
 
   (* Effect of using (copying or moving) a place [pl] on the abstract state [state]. *)
   let move_or_copy pl state =
-    state (* TODO : This code is incorrect. Replace with correct code. *)
+    (* If the type of [pl] is Copy, we may move/copy it with nothing that changes*)
+    if typ_is_copy prog (typ_of_place prog mir pl) then 
+      state
+    (* Otherwise, it is considered unitialized afterwards *)
+    else
+      deinitialize pl state
   in
 
   (* These modules are parameters of the [Fix.DataFlow.ForIntSegment] functor below. *)
   let module Instrs = struct let n = Array.length mir.minstrs end in
+
   let module Prop = struct
     type property = PlaceSet.t
-
     let leq_join p q = if PlaceSet.subset p q then q else PlaceSet.union p q
   end in
+
   let module Graph = struct
     type variable = int
     type property = PlaceSet.t
 
-    (* To complete this module, one can read file active_borrows.ml, which contains a
-      similar data flow analysis. *)
 
+    (* Calls the go function for all entry points with corresponding abstract state 
+    Only entry point = [mir.mentry];
+    with abstract state = [all_places - (mir.locals places and their subplaces)]*)
     let foreach_root go =
-      () (* TODO *)
+      let local_vars = 
+        Hashtbl.fold (fun l _ acc -> PlaceSet.add (PlLocal l) acc) mir.mlocals PlaceSet.empty
+      in
+      (* union of all subplaces of local_vars*)
+      let local_vars_and_subplaces = 
+        PlaceSet.fold (fun pl acc -> PlaceSet.union acc (Hashtbl.find subplaces pl)) local_vars PlaceSet.empty
+      in
+      go mir.mentry (PlaceSet.diff all_places local_vars_and_subplaces)
 
+
+
+    (* The function [foreach_successor] should call the [go] parameter for each successor
+    of [lbl], with the corresponding modified abstract state.
+
+    For a given label, [Fix] will then automatically compute the join of the initial
+    abstract state (if it exists) and of the modified states propagated by each of its
+    predecessors. *)
     let foreach_successor lbl state go =
-        () (* TODO *)
+      (*removes pl and all its subplaces from state*)
+      let assign pl state = 
+        let pl_and_subplaces = Hashtbl.find subplaces pl in
+        PlaceSet.diff state pl_and_subplaces
+      in
+
+      match fst mir.minstrs.(lbl) with
+      | Iassign (pl, RVplace pl1, next) ->
+        let state = move_or_copy pl1 state in
+        go next (assign pl state)
+      | Iassign (pl, _, next) -> go next (assign pl state)
+      | Ideinit (l, next) -> go next (assign (PlLocal l) state)
+      | Igoto next -> go next state
+      | Iif (_, next1, next2) ->
+          go next1 state;
+          go next2 state
+      | Ireturn -> ()
+      | Icall (_, param, pl, next) -> 
+        (* When calling a function, we de-initialise the places of the parameters. *)
+        let state = List.fold_left (fun state pls -> deinitialize pls state ) state param in
+        go next (assign pl state)
+
   end in
   let module Fix = Fix.DataFlow.ForIntSegment (Instrs) (Prop) (Graph) in
-  fun i -> Option.value (Fix.solution i) ~default:PlaceSet.empty
+  
+  fun i -> Option.value (Fix.solution i) ~default:PlaceSet.empty 
